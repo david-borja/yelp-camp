fe63b5fef82dd738db849e1db324de567dd7f455
dav1dborja <davidborjamartinez@gmail.com>
2021-01-29 02:09:30 +0100

    adding cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -19,13 +19,23 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       author: "600ee36203ea6f22ed119097",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
+      price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url:
@@ -38,14 +48,10 @@ const seedDB = async () => {
           filename: "YelpCamp/ujfvnhfxeenohrmxywkj",
         },
       ],
-      price,
-      geometry: { type: "Point", coordinates: [2.17694, 41.3825] },
     });
     await camp.save();
   }
 };
-
-seedDB();
 
 seedDB().then(() => {
   mongoose.connection.close();
